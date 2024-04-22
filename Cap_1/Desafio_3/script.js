a826012dac0e6c10f004ca1955275e619ee6f23c
Pedro <sampaio_pedro@outlook.com>
2024-04-22 17:09:07 -0300

    Atualizando

---
+++ Cap_1/Desafio_3/script.js
@@ -0,0 +1,12 @@
+// let qnt = prompt("Diga o tamanho da sequência:");
+// let aux = (qnt);
+// while (aux >= 0){
+//     alert(qnt - (aux))
+//     aux -= 1
+// }
+
+// let qnt = prompt("Diga o tamanho da sequência:");
+// while (qnt >= 0){
+//     alert(qnt)
+//     qnt -= 1
+// }
