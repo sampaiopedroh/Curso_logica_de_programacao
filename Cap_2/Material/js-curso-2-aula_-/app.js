e58563cc2d319cec0a8d326910eed86251abd2c7
Pedro <sampaio_pedro@outlook.com>
2024-04-23 15:21:13 -0300

    Atualizando

---
+++ Cap_2/Material/js-curso-2-aula_-/app.js
@@ -0,0 +1,16 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
+
+function exibirTexto(tag, texto){
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto;
+}
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10:');
+
+function verificaChute(){
+
+}
