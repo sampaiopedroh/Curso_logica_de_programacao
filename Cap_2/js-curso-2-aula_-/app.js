ab77d0b2e1c17c615d92007c7e72cb382482caed
Pedro <sampaio_pedro@outlook.com>
2024-04-23 21:26:24 -0300

    Atualizando

--- Cap_2/js-curso-2-aula_-/app.js
+++ Cap_2/js-curso-2-aula_-/app.js
@@ -11,6 +11,7 @@ let tentativas = 1;
 function exibirTexto(tag, texto){
     let campo = document.querySelector(tag)
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function mensagemInicial(){
